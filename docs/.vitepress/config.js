44f3c34ced1b7cf6f7f090fa5b1fba1b45e4a102
wuzx <wzx19980321@163.com>
2023-05-09 20:57:44 +0800

    chore: update vitepress1.0.0-alpha.75

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,25 +1,49 @@
-export default {
-	base: '/',
+import { createRequire } from 'module'
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+	// base: '/',
 	title: 'Everything for Frontend', //站点标题
-	description: '与前端有关的一切...', //mate标签description，多用于搜索引擎抓取摘要
+	description: '与前端有关的一切...', //meta标签description，多用于搜索引擎抓取摘要
 	lastUpdated: true,
 	dest: 'public',
+	
 	markdown: {
 		lineNumbers: true, //显示代码行数
 	},
 	themeConfig: {
 		siteTitle: 'Everything for Frontend',
 		// logo: "/logo.png",
-		nav: [{ text: 'github', link: 'https://github.com/magnum-zx' }],
+		nav: nav(),
+
 		socialLinks: [{ icon: 'github', link: 'https://github.com/magnum-zx' }],
-		sidebar: getSidebar(),
+
+		sidebar: {'/articles/': getSidebar()},
+
+		editLink: {
+			pattern: 'https://github.com/magnum-zx/interview-record/edit/main/docs/:path',
+			text: 'Edit this page on GitHub'
+		  },
+
+		footer: {
+		message: 'Released under the MIT License.',
+		copyright: 'Copyright © 2023-present magnum-zx'
+		},
 	},
-}
+})
+
+function nav() {
+	return [
+		{ text: 'Everything', link: '/articles/what-is-everything-for-frontend'},
+		{ text: 'Github', link: 'https://github.com/magnum-zx'},
+	]
+  }
 
 function getSidebar() {
 	return [
 		{
 			text: 'handwriting',
+			collapsed: false,
 			items: [
 				// {
 				// 	text: 'handwriting',
@@ -39,7 +63,7 @@ function getSidebar() {
 				},
 				{
 					text: '实现函数柯里化',
-					link: '/articles/handwriting/curring',
+					link: '/articles/handwriting/currying',
 				},
 				{
 					text: '实现数组拍平',
@@ -49,32 +73,36 @@ function getSidebar() {
 		},
 		{
 			text: 'js',
+			collapsed: false,
 			items: [
 				
 			],
 		},
 		{
 			text: 'css',
+			collapsed: false,
 			items: [
 				
 			],
 		},
 		{
 			text: 'vue',
+			collapsed: false,
 			items: [
 				
 			],
 		},
 		{
 			text: 'react',
+			collapsed: false,
 			items: [
 				
 			],
 		},
 		{
 			text: 'ts',
+			collapsed: false,
 			items: [
-				
 			],
 		},
 
